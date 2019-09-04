0e056b2a837cf119b5978f179f10374adb5e2ef0
Shunta Tokunaga <tokushun109@gmail.com>
2019-09-04 00:17:30 +0000

    currentuser-himodukemae

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
     before_action :set_task,only:[:show, :edit ,:update, :destroy]
+    before_action :require_user_logged_in
     
     def index
         @tasks = Task.all.page(params[:page])
@@ -13,15 +14,16 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(task_params)
-        if @task.save
-            flash[:success] = "正常にタスクを追加できました"
-            redirect_to task_path(@task)
-        else
-            flash.now[:danger] = "タスクの保存に失敗しました"
-                render :new
-        end
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render :show
     end
+  end
 
     def edit
     end
@@ -52,4 +54,7 @@ class TasksController < ApplicationController
         @task = Task.find(params[:id])
     end
 
+    
 end
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
   end
 
   def new
@@ -22,6 +25,7 @@ class UsersController < ApplicationController
       
     end
   end
+  
 end
 
   private
@@ -29,3 +33,4 @@ end
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
